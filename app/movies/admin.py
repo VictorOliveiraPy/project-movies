bbe8a34d1394ed1b88c06e384e4875807fdd4d7d
Victor Hugo <victorblog410@gmail.com>
2021-05-23 20:55:31 -0300

    added pytest serializer and models movies

--- app/movies/admin.py
+++ app/movies/admin.py
@@ -1,3 +1,34 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as DefaultUserAdmin
+
+from .models import Movie, CustomUser
+
 
 # Register your models here.
+
+@admin.register(CustomUser)
+class UserAdmin(DefaultUserAdmin):
+    pass
+
+
+@admin.register(Movie)
+class MovieAdmin(admin.ModelAdmin):
+    fields = (
+        'title',
+        'genre',
+        'year',
+        'created_date',
+        'updated_date',
+        )
+
+    list_display = (
+        'title',
+        'genre',
+        'year',
+        'created_date',
+        'updated_date',
+    )
+    readonly_fields = (
+        'created_date',
+        'updated_date',
+    )
