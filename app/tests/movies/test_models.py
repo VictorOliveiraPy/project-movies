f089b8901c071ca1988e8e503ff8a41c024f8a7b
Victor Hugo <victorblog410@gmail.com>
2021-05-23 22:13:58 -0300

    added test list and detail

--- app/tests/movies/test_models.py
+++ app/tests/movies/test_models.py
@@ -15,3 +15,38 @@ def test_movie_model():
     assert movie.created_date
     assert movie.updated_date
     assert str(movie) == movie.title
+
+
+@pytest.mark.django_db
+def test_add_movie_invalid_json(client):
+    movies = Movie.objects.all()
+    assert len(movies) == 0
+
+    resp = client.post(
+        "/api/movies/",
+        {},
+        content_type="application/json"
+    )
+    assert resp.status_code == 400
+
+    movies = Movie.objects.all()
+    assert len(movies) == 0
+
+
+@pytest.mark.django_db
+def test_add_movie_invalid_json_keys(client):
+    movies = Movie.objects.all()
+    assert len(movies) == 0
+
+    resp = client.post(
+        "/api/movies/",
+        {
+            "title": "The Big Lebowski",
+            "genre": "comedy",
+        },
+        content_type="application/json"
+    )
+    assert resp.status_code == 400
+
+    movies = Movie.objects.all()
+    assert len(movies) == 0
